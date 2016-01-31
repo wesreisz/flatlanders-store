ebbb00cb10c541128d4aaf51aaf7da371af09b6f
Wesley Reisz <wes@wesleyreisz.com>
2016-01-31 16:19:51 -0500

    Created initial project structure
    Used yeoman to generate the project.
    npm install -g grunt-cli bower yo generator-karma generator-angular
    mkdir my-new-project && cd $_
    yo angular gemStore

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name gemStoreApp
+ * @description
+ * # gemStoreApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('gemStoreApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gemStoreApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gemStoreApp
+ */
+angular.module('gemStoreApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
