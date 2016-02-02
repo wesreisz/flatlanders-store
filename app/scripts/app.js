a1a1ab5328179214ca94a17bb939caa19ef719ea
Wesley Reisz <wes@wesleyreisz.com>
2016-02-01 22:39:44 -0500

    Level 3 Completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -30,8 +30,9 @@
     this.review = {};
 
     this.addReview = function(product){
-      product.reviews.push(this.review);
-      this.review = {};
+        this.review.createdOn = Date.now();
+        product.reviews.push(this.review);
+        this.review = {};
     };
   });
 
